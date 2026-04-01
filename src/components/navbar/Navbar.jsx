f23e3db3997eb8459ddc530a9fa79815a702813e
siam-parvez <siamparvez44@gmail.com>
2026-04-01 20:57:49 +0600

    feat: hero section added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,45 +3,76 @@ import logo from "../../assets/logo.png"
 
 const Navbar = () => {
   return (
-    <div className='navbar bg-base-100 shadow-sm'>
-      <div className='flex-1'>
-        <a className='btn btn-ghost text-xl'>
-          <img className='max-w-32' src={logo} alt='' />
-        </a>
-      </div>
-      <div className='flex items-center gap-3'>
-        <div className='indicator'>
-          <ShoppingCart />
-          <span className='badge badge-sm indicator-item'>8</span>
-        </div>
-        <div className='dropdown dropdown-end'>
-          <div
-            tabIndex={0}
-            role='button'
-            className='btn btn-ghost btn-circle avatar'>
-            <div className='w-10 rounded-full'>
-              <img
-                alt='Tailwind CSS Navbar component'
-                src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
-              />
+    <div className='bg-base-100 shadow-sm'>
+      <div className='navbar container mx-auto'>
+        <div className='navbar-start'>
+          <div className='dropdown'>
+            <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'>
+                {' '}
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h8m-8 6h16'
+                />{' '}
+              </svg>
             </div>
+            <ul
+              tabIndex='-1'
+              className='menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow'>
+              <li>
+                <a href='#'>Products</a>
+              </li>
+              <li>
+                <a href='#'>Features</a>
+              </li>
+              <li>
+                <a href='#'>Pricing</a>
+              </li>
+              <li>
+                <a href='#'>Testimonials</a>
+              </li>
+              <li>
+                <a href='#'>FAQ</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex='-1'
-            className='menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow'>
+          <a className='btn btn-ghost text-xl'>
+            <img className='max-w-32' src={logo} alt='digitools logo' />
+          </a>
+        </div>
+        <div className='navbar-center hidden lg:flex'>
+          <ul className='menu menu-horizontal px-1 font-semibold'>
             <li>
-              <a className='justify-between'>
-                Profile
-                <span className='badge'>New</span>
-              </a>
+              <a href='#'>Products</a>
             </li>
             <li>
-              <a>Settings</a>
+              <a href='#'>Features</a>
             </li>
             <li>
-              <a>Logout</a>
+              <a href='#'>Pricing</a>
+            </li>
+            <li>
+              <a href='#'>Testimonials</a>
+            </li>
+            <li>
+              <a href='#'>FAQ</a>
             </li>
           </ul>
+        </div>
+        <div className='navbar-end gap-2'>
+          <div className='indicator'>
+            <ShoppingCart />
+            <span className='badge badge-xs indicator-item hidden'>0</span>
+          </div>
+          <a className='btn btn-ghost'>Login</a>
+          <a className='btn btn-primary'>Get Started</a>
         </div>
       </div>
     </div>
@@ -49,3 +80,47 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+// <div className='navbar bg-base-100 shadow-sm'>
+//       <div className='flex-1'>
+//         <a className='btn btn-ghost text-xl'>
+//           <img className='max-w-32' src={logo} alt='' />
+//         </a>
+//       </div>
+//       <div className='flex items-center gap-3'>
+//         <div className='indicator'>
+//           <ShoppingCart />
+//           <span className='badge badge-sm indicator-item'>8</span>
+//         </div>
+//         <div className='dropdown dropdown-end'>
+//           <div
+//             tabIndex={0}
+//             role='button'
+//             className='btn btn-ghost btn-circle avatar'>
+//             <div className='w-10 rounded-full'>
+//               <img
+//                 alt='Tailwind CSS Navbar component'
+//                 src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+//               />
+//             </div>
+//           </div>
+//           <ul
+//             tabIndex='-1'
+//             className='menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow'>
+//             <li>
+//               <a className='justify-between'>
+//                 Profile
+//                 <span className='badge'>New</span>
+//               </a>
+//             </li>
+//             <li>
+//               <a>Settings</a>
+//             </li>
+//             <li>
+//               <a>Logout</a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
